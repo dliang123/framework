3b407c397788a0ef0a604f0e9c381025c29d4da6
jackdeng <jackdeng@yaomaiche.com>
2017-09-27 18:49:30 +0800

    raw servlet feature

---
+++ framework-web-rawservlet/src/main/java/com/yuntu/servlet/controller/ServletTestController.java
@@ -0,0 +1,43 @@
+package com.yuntu.servlet.controller;
+
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by jackdeng on 2017/9/27.
+ */
+public class ServletTestController extends HttpServlet {
+
+    String message = "hello servlet";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+        ServletInputStream inputStream = request.getInputStream();
+
+        int len = 0;
+        byte[] bytes = new byte[10240];
+        String sss = "";
+        while ((len = inputStream.read(bytes)) != -1) {
+            sss = new String(bytes, 0, len);
+        }
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + sss + "</h1>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        String str = request.getParameter("name");
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + str + "</h1>");
+    }
+}
