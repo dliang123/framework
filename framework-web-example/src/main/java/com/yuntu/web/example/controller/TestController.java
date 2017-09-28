6cd0349c4987fcf1655450b8a9b9865d91acd500
jackdeng <jackdeng@yaomaiche.com>
2017-09-28 18:22:34 +0800

    cache feature

--- framework-web-example/src/main/java/com/yuntu/web/example/controller/TestController.java
+++ framework-web-example/src/main/java/com/yuntu/web/example/controller/TestController.java
@@ -5,30 +5,35 @@ import com.yuntu.base.utils.ConfigUtils;
 import com.yuntu.web.annotation.Cache;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
 /**
- *  15/11/13.
+ * 15/11/13.
  */
 @Controller
 @RequestMapping("test")
-@Cache(cacheKey = "rid")
+@Cache(cacheKey = "Test")
 //@Authority(needAuthority = {"10"})
 public class TestController {
 
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	@ResponseBody
-    @RequestMapping("aaa")
-//    @Cache(cacheKey = "city",scope = Cache.scope.parameter,expire=1000)
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @ResponseBody
+    @RequestMapping(value = "/cacheTest", method = {RequestMethod.GET, RequestMethod.POST})
+    @Cache(cacheKey = "city", scope = Cache.scope.parameter, expire = 1000)
 //    @Degrade(degradeFor = RuntimeException.class)
-    public Object aaa(LogEntity logEntity) throws Exception{
-        System.err.println(logEntity.getRequestTime());
-        Map m = Maps.newConcurrentMap();
-        m.put("m","success");
-        return ConfigUtils.v("a");
+    public String cacheTest(String key) throws Exception {
+
+        Map<String,String> map =Maps.newHashMap();
+        map.put("1","aaa1");
+        map.put("2","bbb");
+        map.put("3","ccc3");
+
+
+        return ConfigUtils.v("a") + map.get(key);
 //        throw new Exception("333333333");
     }
 }
